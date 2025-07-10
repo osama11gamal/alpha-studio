996ddb8ce9d9cb0745208c318b59e6c4e2ed1995
alpha-studio <o.g.esmail@xed.aucegypt.edu>
2025-07-10 11:25:03 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ const BlueWolf = React.lazy(() => import("./pages/BlueWolf"));
 const Join = React.lazy(() => import("./pages/Join"));
 const Purchase = React.lazy(() => import("./pages/Purchase"));
 const Success = React.lazy(() => import("./pages/Success"));
+
 const SerkNosElLeil = React.lazy(() => import("./pages/SerkNosElLeil"));
+const KhidaaAltahror = React.lazy(() => import("./pages/KhidaaAltahror"));
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blue-wolf" element={<BlueWolf />} />
             <Route path="/stories/serk-nos-el-leil" element={<SerkNosElLeil />} />
+            <Route path="/stories/khidaa-altahror" element={<KhidaaAltahror />} />
             <Route path="/join" element={<Join />} />
             <Route path="/purchase/blue-wolf" element={<Purchase />} />
             <Route path="/success" element={<Success />} />
