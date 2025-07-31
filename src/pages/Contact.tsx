d9060a676fa54a62b7a30d4d626df7b98248db79
alpha-studio <o.g.esmail@xed.aucegypt.edu>
2025-07-31 16:38:07 +0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -73,7 +73,7 @@ const Contact = () => {
           className="relative h-[90vh] overflow-hidden"
         >
           <img 
-            src="/osos/vocc.png" 
+            src="/osos/51.jpeg" 
             alt="Contact Alpha Studio" 
             className="w-full h-full object-cover object-center transform scale-105 transition-transform duration-10000 hover:scale-110"
           />
